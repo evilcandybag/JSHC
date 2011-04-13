6989d8c9f50c2e8099a34891d5b0b9a6d8e9f330
Staffan Björnesjö <staffan.bjornesjo@gmail.com>
2011-04-13 09:59:40 +0200

    add utility functions

--- src/Utility/utility.js
+++ src/Utility/utility.js
@@ -1,34 +1,46 @@
 
-// -----------------------------------------------------------------------------
+////////////////////////////////////////////////////////////////////////////////
 
 var JSHC = new Object();
 JSHC.Parser = new Object();
 
 modules = new Object();
 
-// -----------------------------------------------------------------------------
-// including javascript code
+////////////////////////////////////////////////////////////////////////////////
 
-//function include(filename){
-//  document.write("<script src='calculator.js'><\/script>")
-//}
-
-// -----------------------------------------------------------------------------
-// debugging
-
-var getKeys = function(obj){
-   var keys = [];
-   for(var key in obj){
+JSHC.getKeys = function(obj){
+   var key,keys = [];
+   for(key in obj){
       keys.push(key);
    }
    return keys;
-}
+};
 
-var getValues = function(obj){
-   var values = [];
-   for(var key in obj){
+JSHC.getValues = function(obj){
+   var key,values = [];
+   for(key in obj){
       values.push(obj[key]);
    }
    return values;
-}
+};
+
+JSHC.showKeys = function(obj){
+    var key,strs=[];
+    for(k in obj){
+       strs.push(k);
+       strs.push("<br>");
+    }
+    strs.pop();
+    return strs.join("");
+};
+
+JSHC.numberOfKeys = function(obj){
+   var key,keys = 0;
+   for(key in obj){
+      keys = keys + 1;
+   }
+   return keys;
+};
+
+////////////////////////////////////////////////////////////////////////////////
 
