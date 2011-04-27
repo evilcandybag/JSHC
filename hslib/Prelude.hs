7552ac34b115ffbe8491aa756777bf918d199cc4
Staffan Björnesjö <staffan.bjornesjo@gmail.com>
2011-04-27 11:17:44 +0200

    add (+) to prelude

--- hslib/Prelude.hs
+++ hslib/Prelude.hs
@@ -20,9 +20,12 @@ not False = True
 --otherwise :: Bool
 otherwise = True
 
-
 data Maybe a = Nothing | Just a
  -- deriving (Eq, Ord, Read, Show)
 
 data Either a b = Left a | Right b
  -- deriving (Eq, Ord, Read, Show)
+
+
+-- infixl 6 +, -
+(+) = JSHC.Internal.int32add
