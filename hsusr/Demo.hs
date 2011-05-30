342cc165bee82c914ed76edbce4f278dc9348101
PETER HOLM <peterho@ed-5352-04.studat.chalmers.se>
2011-05-30 07:27:01 +0200

    updated demo

--- hsusr/Demo.hs
+++ hsusr/Demo.hs
@@ -1,5 +1,5 @@
-
 module Demo where
+import BinTree
 
 ones = 1 : ones
 twos = map (\ x -> x+1) ones
@@ -8,3 +8,5 @@ l = take 3 twos
 fac 0 = 1
 fac n = n * fac (n-1)
 
+numbers = [2,4,1]
+tree = foldr insert empty numbers
